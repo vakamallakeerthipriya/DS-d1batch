8092657af8ce92124d6f97ae3b7a1192f11fe44e
vakamallakeerthipriya <63597132+vakamallakeerthipriya@users.noreply.github.com>
2020-10-11 22:28:59 -0700

    Add files via upload

---
+++ queueimplementation.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 3
+void insert();
+void delete();
+void display();
+int queue_array[MAX];
+int rear = - 1;
+int front = - 1;
+ int main()
+{
+int choice;
+  while (1)
+{
+printf("1:Insert element \n");
+printf("2:Delete element  \n");
+printf("3:Display the elements \n");
+printf("Enter your choice : ");
+scanf("%d", &choice);
+   switch (choice)
+{
+case 1:
+insert();
+break;
+case 2:
+delete();
+break;
+case 3:
+display();
+break;
+case 4:
+exit(1);
+default:printf("Wrong choice \n");
+} 
+} 
+} 
+void insert()
+{
+int add_item;
+if (rear == MAX - 1)
+printf("Queue Overflow \n");
+    else
+{
+if (front == - 1)
+front = 0;
+printf("Insert the element in queue : ");
+scanf("%d", &add_item);
+rear = rear + 1;
+queue_array[rear] = add_item;
+}
+} 
+void delete()
+{
+if (front == - 1 || front > rear)
+{
+printf("Queue Underflow \n");
+return;
+}
+    else
+{
+printf("Element deleted is: %d\n", queue_array[front]);
+front = front + 1;
+}
+} 
+void display()
+{
+int i;
+if (front == - 1)
+printf("Queue is empty \n");
+    else
+{
+printf("Queue is : \n");
+for (i = front; i <= rear; i++)
+printf("%d ", queue_array[i]);
+printf("\n");
+}
+}
